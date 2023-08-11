fdf9c18bc00fd77dc5d04df7b5aea091e54d67e7
Zain Qureshi <quraishizain500@gmail.com>
2023-08-11 09:48:12 +0500

    My First Comit

---
+++ Pos_Neg_zero.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+	cout<<"Enter a number: ";
+	cin >>a;
+		if (a>0){
+			cout<<"It is a positive number.\n";}
+		else if (a<0){
+			cout<<"It is a negative number.\n";}
+		else {
+			cout<<"It is zero.\n";}
+return 0;
+}
